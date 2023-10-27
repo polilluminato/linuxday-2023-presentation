5a6a10c5dd69b420e515de644b1402bf12b1bd2f
Alberto Bonacina <bonacina.alberto@gmail.com>
2023-10-27 23:29:19 +0200

    feat: Add link to the Flutter Website

--- lib/slides/30_flutter_intro.dart
+++ lib/slides/30_flutter_intro.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_deck/flutter_deck.dart';
+import 'package:linuxday_2023_presentation/slides/ui/ui_link_row.dart';
 import 'package:linuxday_2023_presentation/styles/dimens.dart';
+import 'package:linuxday_2023_presentation/utils/utils.dart';
 
 class FlutterIntroSlide extends FlutterDeckSlideWidget {
   const FlutterIntroSlide()
@@ -14,27 +16,41 @@ class FlutterIntroSlide extends FlutterDeckSlideWidget {
   @override
   FlutterDeckSlide build(BuildContext context) {
     return FlutterDeckSlide.blank(
-      builder: (context) => Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image.asset(
-            'assets/images/flutter-logo.png',
-          ),
-          const SizedBox(height: Dimens.mainMargin),
-          Container(
-            margin: const EdgeInsets.symmetric(
-              horizontal: Dimens.hugeMargin,
-              vertical: Dimens.mainMargin,
+      builder: (context) => Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/flutter-logo.png',
+              width: getScreenWidth(context) * .5,
             ),
-            child: Text(
-              'Framework open source di Google per la creazione di\n'
-              'applicazioni multipiattaforma compilate in modo '
-              'nativo\n da un\'unica base di codice',
-              style: FlutterDeckTheme.of(context).textTheme.subtitle,
-              textAlign: TextAlign.center,
+            const SizedBox(height: Dimens.mainMargin),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: Dimens.hugeMargin,
+                vertical: Dimens.mainMargin,
+              ),
+              child: Text(
+                'Framework open source di Google per la creazione di\n'
+                'applicazioni multipiattaforma compilate in modo '
+                'nativo\n da un\'unica base di codice',
+                style: FlutterDeckTheme.of(context).textTheme.title,
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-        ],
+            UiLinkRow(
+              label: "flutter.dev",
+              padding: const EdgeInsets.only(
+                top: Dimens.mainPadding,
+                bottom: Dimens.mainPadding,
+              ),
+              link: "https://flutter.dev",
+              textStyle: FlutterDeckTheme.of(context).textTheme.subtitle,
+            ),
+          ],
+        ),
       ),
     );
   }
